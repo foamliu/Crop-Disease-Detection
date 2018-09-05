7e1b0aa69b525adb4828ae384adc1243d0e147bc
Yang Liu <foamliu@yeah.net>
2018-09-05 11:20:18 +0800

    update

--- augmentor.py
+++ augmentor.py
@@ -11,7 +11,7 @@ aug_pipe = iaa.Sequential(
         sometimes(iaa.Affine(
             scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},  # scale images to 80-120% of their size, individually per axis
             translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent (per axis)
-            rotate=(-180, 180),  # rotate by -45 to +45 degrees
+            rotate=(-90, 90),  # rotate by -45 to +45 degrees
             shear=(-16, 16),  # shear by -16 to +16 degrees
             order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
             cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
