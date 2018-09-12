95509f94c2e9bc57e76121e7fbdfddeb6038f983
Yang Liu <foamliu@yeah.net>
2018-09-12 13:34:37 +0800

    troubleshooting valid loss high issue

--- model.py
+++ model.py
@@ -2,11 +2,11 @@ from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Model
 
-from config import img_height, img_width, num_classes, FREEZE_LAYERS
+from config import img_height, img_width, num_channels, num_classes, FREEZE_LAYERS
 
 
 def build_model():
-    base_model = InceptionResNetV2(input_shape=(img_height, img_width, 3), weights='imagenet', include_top=False)
+    base_model = InceptionResNetV2(input_shape=(img_height, img_width, num_channels), weights='imagenet', include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     #x = Dropout(0.5)(x)
